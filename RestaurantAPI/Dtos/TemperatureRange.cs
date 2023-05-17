bd11e52c47475d96d608d09db666aa09d2cb4180
Tomasz <tkrol315@gmail.com>
2023-05-17 18:12:23 +0200

    Init

---
+++ RestaurantAPI/Dtos/TemperatureRange.cs
@@ -0,0 +1,8 @@
+﻿namespace RestaurantAPI.Dtos
+{
+    public class TemperatureRange
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+    }
+}
---
+++ RestaurantAPI/WeatherForecastService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI
+{
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+       {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public IEnumerable<WeatherForecast> Get(int toGenerate, int minTemperature, int maxTemperature)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, toGenerate).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(minTemperature, maxTemperature),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
